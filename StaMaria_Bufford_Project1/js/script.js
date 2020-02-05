349a28e78a1295a3881df1eb8a00a22e54e22004
Bufford Brian Sta Maria <iambufford99@gmail.com>
2020-02-05 12:31:29 -0500

    added some jquery fade in

--- StaMaria_Bufford_Project1/js/script.js
+++ StaMaria_Bufford_Project1/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+    $(".bg-overlay").fadeIn(2500);
+    $(".hours").fadeIn(3500);
+    $(".address").hide(0).fadeIn(3500);
+    $(".img-menu").hide(0).fadeIn(3500);
+    $(".social-buttons").hide(0).fadeIn(3500);
+
     $(".icon").click(function() {
         $(".responsive").slideToggle(500);
         $(".responsive").css("display", "flex");
